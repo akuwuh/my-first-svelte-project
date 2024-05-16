e5b48ffdb611f38c0482dadf45ddb2b422250543
cute <cute@cute-2.local>
2024-05-15 20:40:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+my first svelte project! poll app
---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+	import CreatePollForm from "./components/CreatePollForm.svelte";
+
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+	const handleTabChange = (e) => {
+		activeItem = e.detail;
+	}
+</script>
+
+
+<Header />
+<main>
+	<Tabs {activeItem} {items} on:tabChange={handleTabChange}/> 
+	{#if activeItem=== 'Current Polls'}
+		<p>Current Poll</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>Add New Poll</p>
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main {
+		max-width: 960px;
+		margin: 40px auto;
+
+	}
+	
+
+
+</style>
